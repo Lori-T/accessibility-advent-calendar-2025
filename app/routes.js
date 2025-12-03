7dac44cdcf1ac221a561a1805f942a8374790ca7
Lori <121175593+Lori-T@users.noreply.github.com>
2025-12-03 20:47:36 +0000

    initial add

--- app/routes.js
+++ app/routes.js
@@ -7,3 +7,15 @@ const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
 // Add your routes here
+// Homepage – Advent Calendar
+router.get('/', function (req, res) {
+  const today = new Date();
+
+  const currentYear = today.getFullYear();
+  const currentDay = today.getDate();
+
+  res.render('index', {
+    currentYear,
+    currentDay
+  });
+});
